245a68988ec1312d215af4bc2eed2a45d54a60bc
nathaliyax <nathalialimas@hotmail.com>
2026-02-23 11:03:28 -0300

    feature: added relations

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping
+    public ResponseEntity<?> github(){
+        return ResponseEntity.ok("CI/CD via github");
+    }
+}
